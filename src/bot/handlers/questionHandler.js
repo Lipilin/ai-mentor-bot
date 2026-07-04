c3968767363aacab0c361db1110e5950b9dacd52
Егор Липилин <egorlcrab@gmail.com>
2026-07-04 21:47:51 +0300

    fix: исправлен ответ системы при недостатке токенов

--- src/bot/handlers/questionHandler.js
+++ src/bot/handlers/questionHandler.js
@@ -4,6 +4,7 @@ import { Context } from "telegraf"
 import { bot } from "#main"
 import { checkUserTokens, setAccountInfo} from "../services/account/index.js"
 import { Config } from '../../Config.js'
+import { backInlineKeyboard } from '../keyboards/backInlineKeyboard.js'
 
 /**
  * 
@@ -12,15 +13,17 @@ import { Config } from '../../Config.js'
 
 export async function questionHandler(ctx){
     if(checkUserTokens(ctx) && bot.getCurrentStage(ctx) == mentorHandler){
-        ctx.session.user.tokens -= 1
-        await setAccountInfo(ctx.session)
         await ai.peformPipeline(ctx.message.text)
             .then(async (response) => {
+                ctx.session.user.tokens -= 1
+                await setAccountInfo(ctx.session)
                 ctx.reply(response)
             })
             .catch((err) => {
                 console.log(err)
                 ctx.reply(Config.MENTOR_PAGE_PROCESS_ERROR)
             })
+    }else{
+        ctx.reply(Config.MENTOR_PAGE_BLOCK_ERROR, backInlineKeyboard)
     }
 }
