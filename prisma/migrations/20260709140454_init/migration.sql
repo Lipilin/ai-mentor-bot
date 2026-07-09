2e7062046abd024ca1555a7dd3ec769fbcbc900b
Егор Липилин <egorlcrab@gmail.com>
2026-07-09 17:07:07 +0300

    fix(db): изменена структура таблицы users и payment_notes

--- prisma/migrations/20260709140454_init/migration.sql
+++ prisma/migrations/20260709140454_init/migration.sql
@@ -2,22 +2,22 @@
 CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NULL DEFAULT 'ai-mentor-guest',
-    `telegramId` BIGINT NOT NULL,
+    `telegram_id` BIGINT NOT NULL,
     `tokens` INTEGER NOT NULL DEFAULT 0,
 
-    UNIQUE INDEX `users_telegramId_key`(`telegramId`),
+    UNIQUE INDEX `users_telegram_id_key`(`telegram_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `payment_notes` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `userId` INTEGER NOT NULL,
-    `payment_status` ENUM('PENDING', 'PAID', 'FAILED', 'REFUNDED', 'CANCELLED') NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `payment_status` ENUM('PENDING', 'PAID', 'FAILED', 'REFUNDED', 'CANCELLED') NOT NULL DEFAULT 'PENDING',
     `payment_currency` ENUM('RUB', 'USD', 'EUR') NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `payment_notes` ADD CONSTRAINT `payment_notes_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `payment_notes` ADD CONSTRAINT `payment_notes_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
